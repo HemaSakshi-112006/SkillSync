1334acd161e320522698750bdaf306ee03cdc36a
demo-git <snpsuaidshema@gmail.com>
2026-07-15 21:01:29 +0530

    added the authenticatio first step

---
+++ backend/controllers/authController.js
@@ -0,0 +1,9 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+    res.send("Register API Working");
+};
+
+module.exports = {
+    registerUser
+};
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,24 @@ const express = require("express");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
+// Home Route
 app.get("/", (req, res) => {
   res.send("SkillSync Backend Running");
 });
 
+// Start Server
 const startServer = async () => {
   await connectDB();
 
