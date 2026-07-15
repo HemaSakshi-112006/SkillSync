dfb0f18179dca2bb0abbea44b4e12981ad7c3334
demo-git <snpsuaidshema@gmail.com>
2026-07-15 21:45:24 +0530

    did the next step in authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,22 @@
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
-    res.send("Register API Working");
+    const { fullName, username, email, password } = req.body;
+    const existingUser = await User.findOne({
+    $or: [{ email }, { username }]
+});
+
+console.log("existingUser:", existingUser);
+
+if (existingUser) {
+    return res.status(400).json({
+        message: "User already exists"
+    });
+}
+
+    console.log("requestBody:", req.body);
+
+    res.send("Data Received");
 };
 
 module.exports = {
